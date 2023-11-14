239e4cd141303a78399e8b0461549c2bc6094d03
Roman Begorre <bgr4435a@ensfsi.onmicrosoft.com>
2023-11-14 09:38:28 +0100

    Terminer 1

---
+++ src/histoire/HistoireTP5.java
@@ -0,0 +1,29 @@
+package histoire;
+
+import personnages.Commercant;
+import personnages.Ronin;
+import personnages.Yakuza;
+
+public class HistoireTP5 {
+	public static void main(String[] args) {
+		Commercant marco = new Commercant("Marco", "thé", 20);
+		Commercant chonin = new Commercant("Chonin", "thé", 40);
+		Commercant kumi = new Commercant("Kumi", "thé", 10);
+		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+		Ronin roro = new Ronin("Roro", "shochu", 60);
+
+		marco.faireConnaissanceAvec(roro);
+		marco.faireConnaissanceAvec(yaku);
+		marco.faireConnaissanceAvec(kumi);
+		marco.faireConnaissanceAvec(chonin);
+
+		System.out.println("\n");
+
+		marco.listerConnaissance();
+
+		System.out.println("\n");
+
+		yaku.listerConnaissance();
+
+	}
+}
